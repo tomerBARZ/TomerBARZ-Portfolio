75942f8960d0874498950da01fc7762c0e9b1520
tomerBARZ <tomerminecraft9@gmail.com>
2024-11-27 13:36:19 +0200

    Update autoAlignPage.tsx

--- src/pages/autoAlignPage.tsx
+++ src/pages/autoAlignPage.tsx
@@ -8,11 +8,11 @@ const AutoAlignPage = () => {
                 I developed this tool specifically to facilitate the data collection process for our Ace of Aces game. Utilizing OpenCV, it efficiently displays all the scanned pages, automatically identifies key regions of interest, and extracts the relevant sections of the images. The tool also informs users of any discrepancies or missing information from the spreadsheet, highlighting areas that require correction. This streamlined approach not only enhances accuracy but also significantly simplifies the verification process, ensuring that our data remains precise and consistent.
             </p>
             <div style={{ width: "100%", display: "flex", justifyContent: "space-around" }}>
-                <div style={{ display: "flex", flexDirection: "column", maxWidth: "300px", height: "250px", justifyContent: "space-between", textAlign: "center" }}>
+                <div style={{ display: "flex", flexDirection: "column", maxWidth: "22.5vw", justifyContent: "space-between", textAlign: "center" }}>
                     <video src="autoalign_video.mp4" autoPlay loop />
                     <label>Scrolling through all the pages and seperating the numerical data from the graphics</label>
                 </div>
-                <div style={{ display: "flex", flexDirection: "column", maxWidth: "300px", height: "250px", justifyContent: "space-between", textAlign: "center" }}>
+                <div style={{ display: "flex", flexDirection: "column", maxWidth: "22.5vw", justifyContent: "space-between", textAlign: "center" }}>
                     <img src='autoalign_image1.png' />
                     <label>Using the tool to compare discrepancies in the data</label>
                 </div>
