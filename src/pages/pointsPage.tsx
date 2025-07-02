0f56abe9f98eb0a4732912df858e2f5d63d01245
tomerBARZ <tomerminecraft9@gmail.com>
2025-07-02 21:09:12 +0300

    removed points link

--- src/pages/pointsPage.tsx
+++ src/pages/pointsPage.tsx
@@ -5,7 +5,7 @@ const PointsPage = () => {
     return (
         <div style={{ marginBottom: "10vh" }}>
             <p>
-                I developed <a href="https://points.practicalmedia.io" style={{ color: "cyan" }}>Practical Analytics</a>, a streamlined platform designed in collaboration with PracticalNPC, which serves as an all-in-one solution for tracking Twitch viewer engagement through rewards and redeems. This engaging website showcases a leaderboard featuring all connected streamers and their viewers, encouraging healthy competition for the coveted top spot. Since launching the platform and sharing it with streamers in the game development community, we have observed a remarkable increase in reward engagement across their channels, highlighting the positive impact of our innovative tool.
+                I developed <label style={{ color: "cyan" }}>Practical Analytics</label>, a streamlined platform designed in collaboration with PracticalNPC, which serves as an all-in-one solution for tracking Twitch viewer engagement through rewards and redeems. This engaging website showcases a leaderboard featuring all connected streamers and their viewers, encouraging healthy competition for the coveted top spot. Since launching the platform and sharing it with streamers in the game development community, we have observed a remarkable increase in reward engagement across their channels, highlighting the positive impact of our innovative tool.
             </p>
             <div style={{ width: "100%", display: "flex", justifyContent: "space-around" }}>
                 <div style={{ display: "flex", flexDirection: "column", maxWidth: "22.5vw", justifyContent: "space-between", textAlign: "center" }}>
